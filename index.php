bd365bb155cb715d5ba35893eabba10a0c6ff917
atoli07 <alexmaria_2001@hotmail.com>
2022-04-30 09:38:15 -0600

    Update index.php

--- index.php
+++ index.php
@@ -11,30 +11,52 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </head>
 
-<body>
+<body class="container">
     <div id="ascensor">
         <div id="tanques" class="w-100 vh-100d-flex justify-content-center align-items-center">
             <div class="row">
-                <div class="tanque col-4">
-                    <h5>Regular</h5>
-                    <strong>$3.05/galón</strong>
-                    <p>Galones disponibles:</p>
+                <div class="col-4">
+                    <div class="tanque col-4 p-4 mx">
+                        <h5>Regular</h5>
+                        <strong>$3.05/galón</strong>
+                        <p>Galones disponibles:</p>
+                    </div>
                 </div>
-                <div class="tanque col-4">
-                <h5>Especial</h5>
-                    <strong>$3.27/galón</strong>
-                    <p>Galones disponibles:</p>
+                <div class="col-4">
+                    <div class="tanque col-4 p-4 mx">
+                        <h5>Especial</h5>
+                        <strong>$3.27/galón</strong>
+                        <p>Galones disponibles:</p>
+                    </div>
                 </div>
-                <div class="tanque col-4">
-                <h5>Diesel</h5>
-                    <strong>$2.96/galón</strong>
-                    <p>Galones disponibles:</p>
+                <div class="col-4">
+                    <div class="tanque col-4 p-4 mx">
+                        <h5>Diesel</h5>
+                        <strong>$2.96/galón</strong>
+                        <p>Galones disponibles:</p>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div id="mantenimientos" class="w-100 vh-100">
+            <div class="row container-fluid">
+                <table class="table caption-top table-dark">
+                    <caption class="text-white"><center>Ventas</center></caption>
+                    <thead>
+                        <th>ID</th>
+                        <th>Tipo Combustible</th>
+                        <th>Galones vendidos</th>
+                        <th>Monto total</th>
+                        <th>Fecha</th>
+                    </thead>
+                    <tbody>
 
+                    </tbody>
+                </table>
+            </div>
         </div>
+    </div>
+    <div id="mantenimientos" class="w-100 vh-100">
+
+    </div>
     </div>
     <script src="js/jquery.min.js"></script>
     <script src="js/jquery.ascensor.min.js"></script>
